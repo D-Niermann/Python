4ecedc8505464291676399fdd4c98feb30adcacb
D-Niermann <dario.niermann@gmail.com>
2016-12-19 11:49:47 +0100

    Übung 19.12.16
    Lösung des problems für quad und für allgemeine RB

--- FEM Übung.py
+++ FEM Übung.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from scipy import integrate
 import seaborn
 seaborn.set(font_scale=1)
-
+np.set_printoptions(suppress=True)
 seaborn.set_style("ticks",
     {
     'axes.grid': True,
@@ -16,7 +16,8 @@ seaborn.set_style("ticks",
     'legend.frameon': True,
     })
 
-
+def ana(x):
+	return -(x**2)+13.*x-22.
 
 def form1dlin(x,elm_id,node_id,grad=0):
 	"""Erzeugt formfunktion: linierae anpassung zwischen 2 Nodes welche nebeneinander 
@@ -53,7 +54,7 @@ def form1dquad(x,elm_id,node_id,grad=0):
 	x1 = xi[ElmCon[elm_id][0]]
 	x2 = xi[ElmCon[elm_id][1]]
 	x3 = xi[ElmCon[elm_id][2]]
-	h=0.01
+	h=0.001
 
 	if x>xi[ElmCon[elm_id][2]] or x<xi[ElmCon[elm_id][0]]:
 		Ni=0
@@ -76,42 +77,73 @@ def form1dquad(x,elm_id,node_id,grad=0):
 			Ni=(1/((x3-x1)*(x3-x2))*(x+h-x1)*(x+h-x2)-1/((x3-x1)*(x3-x2))*(x-x1)*(x-x2))/h
 		return Ni
 
+def ElmCon(option):
+	ElmCon = []	#ereugt liste mit node id's. xi[ElmCon] gibt dann koordinaten
+	if option=="quad":
+		for  i in range(0,len(xi)-2,2):
+			ElmCon.append([i,i+1,i+2])
+	elif option=="lin":
+		for  i in range(0,len(xi)-1):
+			ElmCon.append([i,i+1])
+	print "ElmCon:",ElmCon
+	return ElmCon
+
 def form_sumquad(x,elm_id):
 	return form1dquad(x,elm_id,0)+form1dquad(x,elm_id,1)+form1dquad(x,elm_id,2)
 
-def form_prod(x,elm_id,Node1,Node2,grad=0):
+def form_prod_lin(x,elm_id,Node1,Node2,grad=0):
 	return form1dlin(x,elm_id,Node1,grad)*form1dlin(x,elm_id,Node2,grad)
+
+def form_prod_quad(x,elm_id,Node1,Node2,grad=0):
+	return form1dquad(x,elm_id,Node1,grad)*form1dquad(x,elm_id,Node2,grad)
 ###########################################################################################
 
 
-################ koordinaten der nodes, muss für quad 2*N+1 sein
-# xi = [2.,4.,6.,8.]
-xi   = np.linspace(0,8,21)
-A    = 10		#Wärmeleitfähigkeit
-k    = 5		#Wärmeleitung
-Q    = 100.		#inhomogenität
-q    = -15.
-ga   = 100
-################# Netz erstellen
-ElmCon = []	#ereugt liste mit node id's. xi[ElmCon] gibt dann koordinaten
-for  i in range(0,len(xi)-1):
-	ElmCon.append([i,i+1])
-print "ElmCon:",ElmCon
 
+
+
+
+################ koordinaten der nodes, muss für quad 2*N+1 sein
+# xi   = [2.,3.,4.,5.,6.,7.,8.]
+xi     = np.linspace(2,8,2*3+1)
+A      = 10		#Wärmeleitfähigkeit
+k      = 5		#Wärmeleitung
+Q      = 100.	#Inhomogenität
+q      = -15.	#Wärmefluss 
+ga     = 10		#Direchlet Temp
+alpha  = 10		#Wärmeübergangskoeff
+T_inf  = 10		#Umgebungstemp
+option = "quad"	#"lin" oder "quad" formfunc auswählen
+##################################################################
+
+
+
+
+################# Wähle lineare oder quadratische formfuntionen
+if option=="lin":
+	formprod=form_prod_lin
+	formfunc=form1dlin
+elif option=="quad":
+	formprod=form_prod_quad
+	formfunc=form1dquad
+
+
+################# Netz erstellen
+ElmCon=ElmCon(option)
 
 
 ################ K Matrix erstellen
 K=np.zeros([len(xi),len(xi)])
 #gehe submatrizen durch ...jede matrix ist ein element zugehörig
 for i in range(len(ElmCon)):	#gehe elemente durch
-	for Node1 in range(len(ElmCon[i])):	#whäle rechte oder linke node
-		for Node2 in range(len(ElmCon[i])):	#wähle andere node
+	for weight in range(len(ElmCon[i])):	#gehe gewichtsfunktionen durch
+		for form in range(len(ElmCon[i])):	#gehe formfunktionen durch
 			a=xi[ElmCon[i][0]]	#grenze für integral
-			b=xi[ElmCon[i][1]]	#grenze für int
-			K[ElmCon[i][Node1],ElmCon[i][Node2]]+=integrate.quad(form_prod,a,b,args=(i,Node1,Node2,1))[0]
+			b=xi[ElmCon[i][-1]]	#grenze für integral
+			K[ElmCon[i][weight],ElmCon[i][form]]+=integrate.quad(formprod,a,b,args=(i,weight,form,1))[0]
 
-K=A*k*K
-print "K-Matrix:\n",K
+K = A*k*K
+print "K-Matrix:\n",np.round(K,1)
 
 
 
@@ -120,9 +152,9 @@ f_L=np.zeros(len(xi))
 for element in range(len(ElmCon)):
 	for Node in range(len(ElmCon[element])):
 		a=xi[ElmCon[element][0]]	#grenze für integral
-		b=xi[ElmCon[element][1]]	#grenze für int
-		f_L[ElmCon[element][Node]]+=Q*integrate.quad(form1dlin,a,b,args=(element,Node))[0]
-print "Lastvektor:\n",f_L
+		b=xi[ElmCon[element][-1]]	#grenze für int
+		f_L[ElmCon[element][Node]]+=Q*integrate.quad(formfunc,a,b,args=(element,Node))[0]
+print "Lastvektor:\n",np.round(f_L,1)
 
 f_Rand=np.zeros(len(xi))
 f_Rand[-1]=q*A
@@ -131,13 +163,32 @@ f=f_L+f_Rand
 
 
 
-############## Löse das Problem
+############# Löse das Problem für direchlet randbedingungen
 """aus der randbed dass T bei x=2 0 ist folgt dass man die erste zeile der K matrix 1,0,0,... 
 setzen kann und den f vektor entssprechend 0 setzt. somit lässt sich die k matrix invertieren
 und das problem lässt sich lösen"""
-K[0,:]=0
-K[0,0]=1
-f[0]=ga
+# K[0,:]=0
+# K[0,0]=1
+# f[0]=ga
+# T=np.linalg.solve(K,f)
+# print "Temp: " , np.round(T,2)
+
+
+############## Löse für wärmeübergangskoeffizeint
+"""ranbedingung bei x=2 ist ein wärmefluss α(T-T_inf) mit T_inf = 0. Da statische 
+berechnung sollte da das selbe rauskommen als wenn man T=0 setzt ...aber für einen 
+eindringenden wärmefluss sollte ein interesantes GGW rauskommen. Da T in der RB 
+gegeben sein muss wird Na=T eingesetzt also ergibt sich mit N und N**T eine neue
+K_C marix welche man zu K anddieren muss."""
+K_C      = np.zeros([np.shape(K)[0],np.shape(K)[1]])
+K_C[0,0] = A*alpha
+
+q_c    = np.zeros(len(xi))
+q_c[0] = T_inf*alpha*A
+
+K = K_C+K
+f = q_c+f
+
 T=np.linalg.solve(K,f)
 print "Temp: " , np.round(T,2)
 
@@ -147,17 +198,17 @@ x_max=xi[-1]
 
 # for el_num in range(3):
 # 	# plt.plot(np.arange(0,x_max,0.05),[form1dlin(x,el_num,2) for x in np.arange(0,x_max,0.05)],"r.-")
-# 	plt.plot(np.arange(0,x_max,0.05),[form1dlin(x,el_num,1) for x in np.arange(0,x_max,0.05)],"g.-")
-# 	plt.plot(np.arange(0,x_max,0.05),[form1dlin(x,el_num,0) for x in np.arange(0,x_max,0.05)],"b.-")
+# 	plt.plot(np.arange(0,x_max,0.05),[form1dquad(x,el_num,1) for x in np.arange(0,x_max,0.05)],"g.-")
+# 	plt.plot(np.arange(0,x_max,0.05),[form1dquad(x,el_num,0) for x in np.arange(0,x_max,0.05)],"b.-")
 	
-	# plt.plot(np.arange(0,x_max,0.05),[form1dlin(x,el_num,2,1) for x in np.arange(0,x_max,0.05)],"r--")
-	# plt.plot(np.arange(0,x_max,0.05),[form1dlin(x,el_num,1,1) for x in np.arange(0,x_max,0.05)],"g--")
-	# plt.plot(np.arange(0,x_max,0.05),[form1dlin(x,el_num,0,1) for x in np.arange(0,x_max,0.05)],"b--")
+# 	plt.plot(np.arange(0,x_max,0.05),[form1dquad(x,el_num,2,1) for x in np.arange(0,x_max,0.05)],"r--")
+# 	plt.plot(np.arange(0,x_max,0.05),[form1dquad(x,el_num,1,1) for x in np.arange(0,x_max,0.05)],"g--")
+# 	plt.plot(np.arange(0,x_max,0.05),[form1dquad(x,el_num,0,1) for x in np.arange(0,x_max,0.05)],"b--")
 	
-	# plt.plot(np.arange(0,x_max,0.1),[form_sum(x,i)     for x in np.arange(0,x_max,0.1)],".-")
 
 # plotte T
 plt.plot(xi,T)
+plt.plot(np.linspace(2,8,100),[ana(x) for x in np.linspace(2,8,100)])
 plt.xlabel("x")
 plt.ylabel("Temperatur "+r"$T$")
 plt.show()
